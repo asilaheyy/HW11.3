b20167740969d739d85b8258419c2063ea653071
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-09-22 17:07:43 +0300

    Сделала ДЗ 3.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // 3.
+    public static void delivery(int deliveryDistance) {
+
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3 ");
+        }
     }
+    public static void main(String[] args) {
+        int deliveryDistance = 95;
+        delivery(deliveryDistance);
+
+    }
+
 }
